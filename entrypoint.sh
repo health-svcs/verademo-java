81088e3b8da2e027543a5b3d00be55ade99dc9d5
cluan02 <andrewcluttey+demo@gmail.com>
2025-12-31 16:45:21 -0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+set -e
 
+# Start MariaDB in the background (provided by base image)
 /usr/local/bin/docker-entrypoint.sh mysqld > /dev/null 2>&1 & disown
-mvn spring-boot:run
+
+# Default app port (can be overridden by APP_PORT env var)
+APP_PORT="${APP_PORT:-8080}"
+
+echo "Starting Spring Boot app on port ${APP_PORT}, binding to 0.0.0.0"
+
+# Run Spring Boot, explicitly binding to all interfaces
+mvn spring-boot:run \
+  -Dspring-boot.run.arguments="--server.port=${APP_PORT} --server.address=0.0.0.0"
